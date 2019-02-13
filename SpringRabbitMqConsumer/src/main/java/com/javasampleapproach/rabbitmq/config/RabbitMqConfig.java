3aa658ae89c158371c314197e1a49f289e95a3e8
Nalinshankar <nalinmakaut@gmail.com>
2019-02-13 09:40:25 +0530

    updated

--- SpringRabbitMqConsumer/src/main/java/com/javasampleapproach/rabbitmq/config/RabbitMqConfig.java
+++ SpringRabbitMqConsumer/src/main/java/com/javasampleapproach/rabbitmq/config/RabbitMqConfig.java
@@ -2,9 +2,13 @@ package com.javasampleapproach.rabbitmq.config;
 
 import org.springframework.amqp.core.*;
 
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,6 +32,20 @@ public class RabbitMqConfig {
     Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory jsaFactory(ConnectionFactory connectionFactory,
+                                                           SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter());
+        return factory;
+    }
+
 
     @Bean
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
--- SpringRabbitMqProducer/src/main/java/com/javasampleapproach/rabbitmq/config/RabbitMqConfig.java
+++ SpringRabbitMqProducer/src/main/java/com/javasampleapproach/rabbitmq/config/RabbitMqConfig.java
@@ -1,15 +1,15 @@
 package com.javasampleapproach.rabbitmq.config;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 
 
-
-    @Configuration
+@Configuration
     public class RabbitMqConfig {
 
         public static final String ROUTING_KEY = "my.queue.key";
@@ -27,6 +27,16 @@ import org.springframework.amqp.core.TopicExchange;
         @Bean
         Binding binding(Queue queue, TopicExchange exchange) {
             return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+        }
+        @Bean
+        public MessageConverter jsonMessageConverter(){
+            return new Jackson2JsonMessageConverter();
+        }
+
+        public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+            final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+            rabbitTemplate.setMessageConverter(jsonMessageConverter());
+            return rabbitTemplate;
         }
 
     }
